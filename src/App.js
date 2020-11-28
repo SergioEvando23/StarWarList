f67dd194f3231d3e3e52743d3f93ea2374b991df
SergioEvando23 <sergioevandocosta@gmail.com>
2020-11-28 14:12:42 -0300

    pre-definition of the app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from "react";
 import './App.css';
 import DetailScreen from './DetailScreen/DetailScreen';
 import ListScreen from './ListScreen/ListScreen';
+import Button from '@material-ui/core/Button';
+import HomeIcon from '@material-ui/icons/Home';
+import EventSeatIcon from '@material-ui/icons/EventSeat';
+import PublicIcon from '@material-ui/icons/Public';
 
 function App() {
   return (
     <div className="app">
       <div className="app__header"> 
-       App
+        <div className="app__app-header__buttons"> 
+        <Button variant="outlined" color="primary" > <HomeIcon/>HOME </Button>
+        <Button variant="outlined" color="primary" > <PublicIcon/>PLANET </Button>
+        <Button variant="outlined" color="primary" > <EventSeatIcon/>VEHICLE </Button>
+        </div> 
+        <div className="app__app-header__app-header-img">
+          StarWars
+        </div>
       </div> 
       <div className="app__pages">
         <ListScreen />
